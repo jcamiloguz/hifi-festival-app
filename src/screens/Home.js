8a17bccb3cd2165b015b38589225e35c8ef0ef97
Juan Felipe Peña Vacilescu <48024624+jfelipepv@users.noreply.github.com>
2022-05-27 21:47:16 -0500

    Location festival

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Text } from "react-native"
+import { Location } from "../components/Location"
 
 export function Home() {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <Location></Location>
     </SafeAreaView>
   )
 }
