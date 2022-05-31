2deb597591ad429b2c9f7aa4158e1a400b649ce1
jcamiloguz <jcamiloguz1@gmail.com>
2022-05-30 19:23:37 -0500

    menu icons and images

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import * as React from "react"
 import { Artists } from "./src/screens/Artists"
 import { Gallery } from "./src/screens/Gallery"
 import { Home } from "./src/screens/Home"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+import IconSecond from "react-native-vector-icons/FontAwesome"
 import { NavigationContainer } from "@react-navigation/native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { User } from "./src/screens/User"
@@ -19,10 +21,43 @@ export default function App() {
             headerShown: false,
           }}
         >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Artists" component={Artists} />
-          <Tab.Screen name="Gallery" component={Gallery} />
-          <Tab.Screen name="User" component={User} />
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Artists"
+            component={Artists}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="account-music" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Gallery"
+            component={Gallery}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="view-carousel" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="User"
+            component={User}
+            options={{
+              tabBarLabel: "Tickets",
+              tabBarIcon: ({ color, size }) => (
+                <IconSecond name="ticket" color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </SafeAreaProvider>
     </NavigationContainer>
